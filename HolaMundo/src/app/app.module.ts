f42b20ade5b9b04d6beca625ad31f2544bde57e4
jose bascuñan <jose.bascunan.p@gmail.com>
2017-08-13 19:53:39 -0400

    modifica estructura

--- HolaMundo/src/app/app.module.ts
+++ HolaMundo/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header.component';
-import { BodyComponent } from "./components/body.component";
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from "./components/body/body.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
